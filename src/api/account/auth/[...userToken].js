6038448105ff511b7cb6fc1eeb8fa22e8f014b83
Kyle Mathews <mathews.kyle@gmail.com>
2021-12-02 16:27:43 -0800

    gah

--- src/api/account/auth/[...userToken].js
+++ src/api/account/auth/[...userToken].js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
   console.log(req.params)
-  res.json(params)
+  res.json(req.params)
 
   // `src/api/foo/1/2 // params[0] === `1`
   // params[1] === `2`
